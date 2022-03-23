fa124439a289be38744174210458f38baad84c1c
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-03-23 18:13:05 -0500

    add forecast elements and styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,22 +5,32 @@ import { isValidUSZipcode } from '@utils/isValidUSZipcode'
 
 const App = () => {
   const [zipcode, setZipcode] = useState('')
-  const [forecast, setForecast] = useState(null)
+  const [error, setError] = useState(null)
+  const [forecasts, setForecasts] = useState(null)
+
+  const AWSEndpoint = (zipcode) =>
+    `https://lfl3ntnyl2.execute-api.us-east-1.amazonaws.com/forecast?zipcode=${zipcode}`
 
   const handleForecastSearch = async () => {
     if (zipcode === '') {
-      setForecast('please enter a US zipcode')
+      setError('please enter a US zipcode')
       return
     }
     if (!isValidUSZipcode(zipcode)) {
-      setForecast(`${zipcode} is not a valid US zipcode`)
+      setError(`${zipcode} is not a valid US zipcode`)
       return
     }
     if (isValidUSZipcode(zipcode)) {
-      setForecast(zipcode)
+      setError(null)
+      const response = await fetch(AWSEndpoint(zipcode))
+      const responseData = await response.json()
+      setForecasts(responseData.data.timelines)
       setZipcode('')
     }
   }
+
+  const parseDate = (date) =>
+    `${new Date(date).getMonth()}/${new Date(date).getDate()}`
 
   return (
     <Layout>
@@ -29,13 +39,6 @@ const App = () => {
       </h1>
       <div className="flex flex-col items-center gap-4 p-4 rounded-xl shadow-xl bg-white">
         <h2 className="text-xl text-gray-600">search by zipcode</h2>
-        {/* <input
-          className="bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400"
-          type="text"
-          placeholder="zipcode..."
-          value={zipcode}
-          onChange={(e) => setZipcode(e.target.value)}
-        /> */}
         <select
           name="zipcode"
           className="border-2 border-blue-200 outline-none px-4 py-2 rounded-lg text-gray-700 cursor-pointer shadow-lg hover:shadow-xl transition"
@@ -54,9 +57,38 @@ const App = () => {
           search
         </div>
       </div>
-      {forecast && <div>{forecast}</div>}
+      {error && <div>{error}</div>}
+      <Forecast date="date" forecast="forecast" temp="temp" wind="wind" />
+      {forecasts && (
+        <>
+          <Forecast
+            date={parseDate(forecasts[0].intervals[0].startTime)}
+            forecast={forecasts[0].intervals[0].values.weatherCode}
+            temp={forecasts[0].intervals[0].values.temperature}
+            wind={forecasts[0].intervals[0].values.windSpeed}
+          />
+          {forecasts[1].intervals.map((forecast, i) => (
+            <Forecast
+              key={i}
+              date={parseDate(forecast.startTime)}
+              forecast={forecast.values.weatherCode}
+              temp={forecast.values.temperature}
+              wind={forecast.values.windSpeed}
+            />
+          ))}
+        </>
+      )}
     </Layout>
   )
 }
 
 export default App
+
+const Forecast = ({ date, forecast, temp, wind }) => (
+  <div className="flex flex-row items-center justify-around gap-4 p-4 rounded-xl shadow-xl bg-white">
+    <h3>{date}</h3>
+    <h3>{forecast}</h3>
+    <h3>{temp}</h3>
+    <h3>{wind}</h3>
+  </div>
+)
