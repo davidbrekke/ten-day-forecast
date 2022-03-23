57ee189ed749e5553451bbb11e6a1b47bcbf1a78
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-03-23 18:13:50 -0500

    add serverless backend

---
+++ backend/lambdas/api-responses.js
@@ -0,0 +1,28 @@
+const responses = {
+  // Lambda response for successful API call
+  _200(data = {}) {
+    return {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:3000',
+        'Access-Control-Allow-Methods': 'GET',
+      },
+      statusCode: 200,
+      body: JSON.stringify(data),
+    }
+  },
+  // Lambda response for failed API call
+  _400(data = {}) {
+    return {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:3000',
+        'Access-Control-Allow-Methods': 'GET',
+      },
+      statusCode: 400,
+      body: JSON.stringify(data),
+    }
+  },
+}
+
+module.exports = responses
