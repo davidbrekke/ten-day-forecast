d2aea3be5b8a8c81cd5bfd2cc26a01358da0afe9
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-03-26 10:37:26 -0500

    return awsEndpoint based on NODE_ENV

--- packages/frontend/src/utils/awsEndpoint.js
+++ packages/frontend/src/utils/awsEndpoint.js
@@ -1,4 +1,18 @@
-const awsEndpoint = (zipcode) =>
-  `https://lfl3ntnyl2.execute-api.us-east-1.amazonaws.com/forecast?zipcode=${zipcode}`
+import { NODE_ENV } from '@utils/constants'
+
+const testEndpoint =
+  'https://ev57rp9uzc.execute-api.us-east-1.amazonaws.com/forecast'
+
+const prodEndpoint =
+  'https://dn8amkrgu2.execute-api.us-east-1.amazonaws.com/forecast'
+
+const awsEndpoint = (zipcode) => {
+  if (NODE_ENV === 'development') {
+    return `${testEndpoint}?zipcode=${zipcode}`
+  }
+  if (NODE_ENV === 'production') {
+    return `${prodEndpoint}?zipcode=${zipcode}`
+  }
+}
 
 export { awsEndpoint }
