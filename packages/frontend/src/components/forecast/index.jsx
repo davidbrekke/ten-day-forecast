266837dfaee7b6bd715e08afdcfcfcaa01209ee3
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-03-26 17:15:29 -0500

    update ui for updated forecast return data

--- packages/frontend/src/components/forecast/index.jsx
+++ packages/frontend/src/components/forecast/index.jsx
@@ -1,25 +1,15 @@
-import CurrentForecast from './CurrentForecast'
 import DayForecast from './DayForecast'
 import { parseDate } from '@utils/parseDate'
 
-const Forecast = ({ forecasts }) => (
-  <>
-    <CurrentForecast
-      date={parseDate(forecasts[1].intervals[0].startTime)}
-      forecast={forecasts[0].intervals[0].values.weatherCode}
-      temp={forecasts[0].intervals[0].values.temperature}
-      wind={forecasts[0].intervals[0].values.windSpeed}
+const Forecast = ({ forecasts }) =>
+  forecasts[0].intervals.map((forecast, i) => (
+    <DayForecast
+      key={i}
+      date={parseDate(forecast.startTime)}
+      forecast={forecast.values.weatherCode}
+      temp={forecast.values.temperature}
+      wind={forecast.values.windSpeed}
     />
-    {forecasts[0].intervals.map((forecast, i) => (
-      <DayForecast
-        key={i}
-        date={parseDate(forecast.startTime)}
-        forecast={forecast.values.weatherCode}
-        temp={forecast.values.temperature}
-        wind={forecast.values.windSpeed}
-      />
-    ))}
-  </>
-)
+  ))
 
 export default Forecast
