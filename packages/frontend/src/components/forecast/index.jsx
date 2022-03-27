76cc3bb5c32d2faec52ad40fb333987a92e8e3e0
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-03-26 21:35:23 -0500

    add weekday and update props

--- packages/frontend/src/components/forecast/index.jsx
+++ packages/frontend/src/components/forecast/index.jsx
@@ -1,12 +1,24 @@
 import DayForecast from './DayForecast'
 import { parseDate } from '@utils/parseDate'
+import { getDayOfWeek } from '@utils/parseDate'
+
+const weekday = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+]
 
 const Forecast = ({ forecasts }) =>
   forecasts[0].intervals.map((forecast, i) => (
     <DayForecast
       key={i}
       date={parseDate(forecast.startTime)}
-      forecast={forecast.values.weatherCode}
+      dayOfWeek={weekday[getDayOfWeek(forecast.startTime)]}
+      weatherCode={forecast.values.weatherCode}
       temp={forecast.values.temperature}
       wind={forecast.values.windSpeed}
     />
