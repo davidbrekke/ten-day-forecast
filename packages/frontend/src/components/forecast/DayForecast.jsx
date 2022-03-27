257f4d14b7d30b3c7dd47f859fbaf17c84e1aab1
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-03-26 21:35:39 -0500

    update forecast styles

--- packages/frontend/src/components/forecast/DayForecast.jsx
+++ packages/frontend/src/components/forecast/DayForecast.jsx
@@ -1,9 +1,16 @@
-const DayForecast = ({ date, forecast, temp, wind }) => (
-  <div className="flex flex-row items-center justify-around gap-4 p-4 rounded-xl shadow-xl text-gray-600 bg-white cursor-pointer transition hover:shadow-2xl hover:scale-105">
-    <h3>{date}</h3>
-    <h3>{forecast}</h3>
-    <h3>{temp}</h3>
-    <h3>{wind}</h3>
+import { weatherCodes } from '@utils/weatherCodes'
+
+const DayForecast = ({ date, dayOfWeek, weatherCode, temp, wind }) => (
+  <div className="flex flex-row items-center justify-evenly w-1/2 min-w-fit p-4 rounded-xl shadow-xl text-gray-600 bg-white cursor-pointer transition hover:shadow-2xl hover:scale-105">
+    <div className="flex flex-col items-center">
+      <h3 className="font-bold">{dayOfWeek.substring(0, 3)}</h3>
+      <h3 className="text-gray-500">{date}</h3>
+    </div>
+    <h3 className="text-center px-4">{weatherCodes[weatherCode]}</h3>
+    <div className="flex flex-col items-center">
+      <h3 className="font-bold text-lg">{Math.round(temp)}°</h3>
+      <h3 className="text-gray-500 text-center">{Math.round(wind)} mph</h3>
+    </div>
   </div>
 )
 
